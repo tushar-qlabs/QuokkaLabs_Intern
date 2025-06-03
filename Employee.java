e395a2d8c7a4b8637df819cbccb9c5e2b5dc8e8a
tushar-qlabs <tushar.kumar@quokkalabs.com>
2025-06-03 11:57:38 +0530

    Update Employee.java

--- Employee.java
+++ Employee.java
@@ -19,7 +19,7 @@ public class Employee {
     public void displayDetails() {
         System.out.println("ID: " + id);
         System.out.println("Name: " + name);
-        System.out.println("Salary: " + salary);
+        System.out.println("Salary: " + getSalary());
         System.out.println("Department: " + department);
     }
 }
